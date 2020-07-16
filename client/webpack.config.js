00fb760a422d442934200ceaa19a91874c2c0fed
younho9 <younho9.choo@gmail.com>
2020-07-16 16:27:42 +0900

    feat: 실행 모드에 따른 웹팩 설정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,28 +4,33 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const banner = require('./banner');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/main.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+module.exports = env => {
+  return {
+    mode: env.mode,
+    entry: {
+      main: './src/main.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve('./dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        templateParameters: {
+          env: env.mode === 'development' ? '(개발용)' : '', 
+        },
+      }),
+      new webpack.BannerPlugin(banner),
+      new CleanWebpackPlugin()
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new webpack.BannerPlugin(banner),
-    new CleanWebpackPlugin()
-  ]
+  }
 };
