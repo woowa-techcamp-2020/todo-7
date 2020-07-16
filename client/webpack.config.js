e9fdde0418e7de51f15230d3c6e3d8df1a027a7f
younho9 <younho9.choo@gmail.com>
2020-07-16 10:36:03 +0900

    feat: webpack config 기본 설정 추가

---
+++ client/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/main.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+  },
+};
