342316deb004524786348ba3632c3fb6fd40c128
zoomkoding <zoomkoding@gmail.com>
2020-07-19 15:01:08 +0900

    feat: card 구현(추가적으로 개선 여지 많습니다!)
    - project-column-card (기본 노트 카드)
    - project-column-new-card (노트 작성할 때 쓰는 카드)

--- client/src/components/molecules/card/index.js
+++ client/src/components/molecules/card/index.js
@@ -1,7 +1,54 @@
-import element from "../../element.js";
-
 import "./styles.css";
-import button from "../../atoms/button/index.js";
-
-export default (type) => element(`${type}-card`,`
+import element from "../../element";
+import button from "../../atoms/button";
+import textarea from "../../atoms/textarea";
+import icon from "../../atoms/icon";
+const addButton = () => button('primary', 'Add', true);
+const cancelButton = () => button('dismiss', 'Cancel');
+const cardButtonBar = () => `${addButton()}${cancelButton()}`;
+const deleteIcon = () => icon('');
+const header = (type, text) => element(
+    `${type}-card-header`, 
+    `${element(
+        `${type}-card-header-text`, 
+        text)
+    }
+    ${deleteIcon()}
 `);
+
+const body = (type, text) => element(
+    `${type}-card-body`, 
+    text
+);
+
+const footer = (type, child) => element(
+    `${type}-card-footer`,
+    child
+);
+
+const projectColumnCard = (type, data) => `
+    ${header(type, data.title)}
+    ${body(type, data.description)}
+    ${footer(type, `Added by ${element(
+        `${type}-card-footer-writer`, 
+        data.writer)}
+    `)}
+`;
+
+const projectColumnNewCard = (type) => `
+    ${textarea(`${type}-card`, 'note', 'Enter a note')}    
+    ${footer(type, cardButtonBar())}
+`
+
+const getCardByType = (type, data) => {
+    console.log(data);
+    switch(type){
+        case 'project-column-new' :
+            return `${projectColumnNewCard(type)}`
+        case 'project-column' :
+            return projectColumnCard(type, data);
+    }
+    
+}
+
+export default (type, data) => element(`${type}-card`, getCardByType(type, data));
