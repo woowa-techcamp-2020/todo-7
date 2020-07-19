df1b41d44c7db535f6bb3d94dca4d207b82f104d
younho9 <younho9.choo@gmail.com>
2020-07-19 18:48:02 +0900

    feat: events 라우터 작성

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// const eventsController = require('../controllers/events');
+const eventsController = require('../controllers/events');
+const { wrapAsync } = require('../utils/helper');
+
+router.post('/', wrapAsync(eventsController.create));
+router.get('/:id', wrapAsync(eventsController.findById));
 
 module.exports = router;
