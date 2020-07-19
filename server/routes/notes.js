c8f20bbaa63704958765c1fb78a209a9a5c89aaf
younho9 <younho9.choo@gmail.com>
2020-07-19 18:42:00 +0900

    feat: notes 라우터 작성

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// const notesController = require('../controllers/notes');
+const notesController = require('../controllers/notes');
+const { wrapAsync } = require('../utils/helper');
+
+router.post('/', wrapAsync(notesController.create));
+router.get('/:id', wrapAsync(notesController.findById));
+router.put('/', wrapAsync(notesController.update));
+router.delete('/:id', wrapAsync(notesController.delete));
 
 module.exports = router;
