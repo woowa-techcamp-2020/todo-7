ae50b79c67e9541d4c10ab88aa5e38623d771eb5
zoomkoding <zoomkoding@gmail.com>
2020-07-19 15:05:05 +0900

    feat: columns은 organisms로 home 페이지는 project 페이지로 변경

--- client/src/components/molecules/column/index.js
+++
@@ -1,9 +0,0 @@
-import element from "../../element.js";
-import card from "../card/index.js";
-import columnHeader from "../columnHeader/index.js";
-import "./styles.css";
-
-export default (type, title, data) => element(`${type}-column`, `
-    ${columnHeader(type, title, data.length)}
-    ${data.reduce((cards, data) => (cards + card(`${type}-column`, data)), '')}
-`);
