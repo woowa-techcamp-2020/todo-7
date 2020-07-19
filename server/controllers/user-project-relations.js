2bce8e341825b8f9d572ba409292e92789b6bb19
younho9 <younho9.choo@gmail.com>
2020-07-20 00:07:36 +0900

    feat: UserProjectRelations 컨트롤러 추가

---
+++ server/controllers/user-project-relations.js
@@ -0,0 +1,25 @@
+const UserProjectRelations = require('../models/user-project-relations');
+
+exports.create = async (req, res) => {
+  const userProjectRelation = await UserProjectRelations.create(req.body);
+  res.send(userProjectRelation);
+};
+
+exports.findById = async (req, res) => {
+  const userProjectRelation = await UserProjectRelations.findOne('*', {
+    id: req.params.id,
+  });
+  if (!userProjectRelation)
+    res.status(404).send('user project relation not found');
+  else res.send(userProjectRelation);
+};
+
+exports.update = async (req, res) => {
+  await UserProjectRelations.update(req.body);
+  res.send('successfully updated');
+};
+
+exports.delete = async (req, res) => {
+  await UserProjectRelations.delete(req.params.id);
+  res.send('successfully deleted');
+};
