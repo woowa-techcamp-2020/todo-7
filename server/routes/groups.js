00d2a9cbc4e2872f606e6fb3d69808fb342c0868
younho9 <younho9.choo@gmail.com>
2020-07-19 18:43:46 +0900

    feat: groups 라우터 작성

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// const columnsController = require('../controllers/groups');
+const groupsController = require('../controllers/groups');
+const { wrapAsync } = require('../utils/helper');
+
+router.post('/', wrapAsync(groupsController.create));
+router.get('/:id', wrapAsync(groupsController.findById));
+router.put('/', wrapAsync(groupsController.update));
+router.delete('/:id', wrapAsync(groupsController.delete));
 
 module.exports = router;
