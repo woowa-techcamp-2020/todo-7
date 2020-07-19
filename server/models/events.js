f566b7aa080d3794117479f2161de8cd35d224a9
younho9 <younho9.choo@gmail.com>
2020-07-19 23:15:26 +0900

    fix: Events 모델 데이터 수정
    projectId, description을 required: true로 변경

--- server/models/events.js
+++ server/models/events.js
@@ -4,8 +4,8 @@ class Events extends Model {
   static init() {
     return super.init({
       id: { dataType: 'int', required: false },
-      projectId: { dataType: 'int', required: false },
-      description: { dataType: 'text', required: false },
+      projectId: { dataType: 'int', required: true },
+      description: { dataType: 'text', required: true },
       createdAt: { dataType: 'datetime', required: false },
       updatedAt: { dataType: 'datetime', required: false },
     });
