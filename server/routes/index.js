263684e89fbb5f303bae3c7050c1bb91e4d4a4c2
younho9 <younho9.choo@gmail.com>
2020-07-20 00:08:16 +0900

    feat: UserProjectRelation 라우터 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const usersRouter = require('./users');
 const projectsRouter = require('./projects');
+const userProjectRelationsRouter = require('./user-project-relations');
 const groupsRouter = require('./groups');
 const notesRouter = require('./notes');
 const eventsRouter = require('./events');
 
 router.use('/users', usersRouter);
 router.use('/projects', projectsRouter);
+router.use('/user-project-relations', userProjectRelationsRouter);
 router.use('/groups', groupsRouter);
 router.use('/notes', notesRouter);
 router.use('/events', eventsRouter);
