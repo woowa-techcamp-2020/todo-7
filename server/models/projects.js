630d0fb65a0bdf86961fadfd7a35bd02919ffe4a
younho9 <younho9.choo@gmail.com>
2020-07-21 02:36:02 +0900

    feat: getProject API 함수 작성 완료

--- server/models/projects.js
+++ server/models/projects.js
@@ -15,9 +15,9 @@ class Projects extends Model {
   static async findById(where) {
     const validatedWhere = this.validate({ ...where, ...this.defaultWhere });
     const queryStmt = `
-      SELECT Projects.id AS 'project.id', Projects.title AS 'project.title', Projects.description AS 'project.description', 
-             Groups.id AS 'group.id', Groups.title AS 'group.title', 
-             Notes.id AS 'group.note.id', Notes.title AS 'group.note.title', Notes.description AS 'group.note.description'
+      SELECT Projects.id AS 'projectId', Projects.title AS 'projectTitle', Projects.description AS 'projectDescription', 
+             Groups.id AS 'groupId', Groups.title AS 'groupTitle', 
+             Notes.id AS 'noteId', Notes.title AS 'noteTitle', Notes.description AS 'noteDescription'
       FROM Projects 
       INNER JOIN Groups ON Groups.projectId = Projects.id
       INNER JOIN Notes ON Notes.groupId = Groups.id
@@ -26,7 +26,50 @@ class Projects extends Model {
         .join(' AND ')}
       ORDER BY Groups.id, Notes.id
     `;
-    return (await this.pool.query(queryStmt))[0];
+    const result = (await this.pool.query(queryStmt))[0];
+    return this.parseData(result);
+  }
+
+  static parseData(data) {
+    const project = {};
+    let prevGroupId;
+    let prevGroupTitle;
+    let prevGroupNotes = [];
+
+    data.map((obj, idx, arr) => {
+      if (idx === 0) {
+        project.id = obj.projectId;
+        project.title = obj.projectTitle;
+        project.description = obj.projectDescription;
+        project.groups = [];
+        prevGroupId = obj.groupId;
+        prevGroupTitle = obj.groupTitle;
+      }
+      if (prevGroupId !== obj.groupId) {
+        project.groups.push({
+          id: prevGroupId,
+          title: prevGroupTitle,
+          notes: prevGroupNotes.slice(0),
+        });
+        prevGroupId = obj.groupId;
+        prevGroupTitle = obj.groupTitle;
+        prevGroupNotes = [];
+      }
+      prevGroupNotes.push({
+        id: obj.noteId,
+        title: obj.noteTitle,
+        description: obj.noteDescription,
+      });
+      if (arr.length === idx + 1) {
+        project.groups.push({
+          id: prevGroupId,
+          title: prevGroupTitle,
+          notes: prevGroupNotes.slice(0),
+        });
+      }
+    });
+
+    return project;
   }
 }
 
