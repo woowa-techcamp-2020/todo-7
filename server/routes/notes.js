848e81e1b11fe7de6f97818e68487bacc9eba858
younho9 <younho9.choo@gmail.com>
2020-07-22 03:18:57 +0900

    feat: 라우터에 moveNotes 추가

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -7,6 +7,7 @@ const { wrapAsync } = require('../utils/helper');
 router.post('/', wrapAsync(notesController.create));
 router.get('/:id', wrapAsync(notesController.findById));
 router.put('/', wrapAsync(notesController.update));
+router.put('/move', wrapAsync(notesController.move));
 router.delete('/:id', wrapAsync(notesController.delete));
 
 module.exports = router;
