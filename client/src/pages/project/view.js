2201bb0296c28718bbf4cd32b275d619fd6669ad
zoomkoding <zoomkoding@gmail.com>
2020-07-23 17:00:08 +0900

    feat: drag에 전달할 ondragend 함수 구현 및 이벤트 추가

--- client/src/pages/project/view.js
+++ client/src/pages/project/view.js
@@ -19,16 +19,32 @@ export default class ProjectView {
     this.deleteNoteEvent = new Event();
     this.createGroupEvent = new Event();
     this.deleteGroupEvent = new Event();
+    this.moveNoteEvent = new Event();
+    this.moveGroupEvent = new Event();
   }
 
   addUserEventListener() {
+    const self = this;
     this.app.addEventListener('contextmenu', (event) => {
       event.preventDefault();
       return false;
     });
     this.app.addEventListener('click', (event) => this.onAppClickHandler(event, this));
-    new DragAndDrop(this.app.querySelector('.project'), '.project-column-body', '.project-column-card');
-    new DragAndDrop(this.app.querySelector('.project'), '.project-columns', '.project-column');
+
+    new DragAndDrop({
+      container: this.app.querySelector('.project'),
+      parentSelector: '.project-column-body',
+      childSelector: '.project-column-card',
+      onDragEnd: (card) => self.onCardDragEndHandler(card),
+    });
+
+    new DragAndDrop({
+      container: this.app.querySelector('.project'),
+      parentSelector: '.project-columns',
+      childSelector: '.project-column',
+      onDragEnd: (card) => self.onColumnDragEndHandler(card),
+    });
+
     const columns = document.querySelectorAll('.project-column');
     columns.forEach((column) => {
       column.addEventListener('mousedown', (event) => this.onColumnClickHandler(event, this));
@@ -63,6 +79,15 @@ export default class ProjectView {
 
     if (foundHandler) event.stopImmediatePropagation();
   }
+  onCardDragEndHandler(card) {
+    this.moveNoteEvent.trigger({
+      id: getNumber(card.id),
+      targetId: getNumber(card.previousElementSibling.id),
+      groupId: getNumber(card.closest('.project-column').id),
+    });
+  }
+
+  onColumnDragEndHandler(column) {}
 
   render() {
     this.app.innerHTML = projectPage(this.project);
@@ -92,10 +117,10 @@ export default class ProjectView {
     );
   }
 
-  updateColumnCounter({ note, noteCount }) {
-    const columnId = note.groupId;
+  updateColumnCounter(columnId) {
     const column = this.app.querySelector(`#project-column-${columnId}`);
-    column.querySelector('.project-column-header-counter').innerHTML = noteCount;
+    const count = column.querySelector('.project-column-body').childElementCount;
+    column.querySelector('.project-column-header-counter').innerHTML = count - 1;
   }
 
   updateCard(data) {}
