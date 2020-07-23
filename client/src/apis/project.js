791ea745f1e5db4e0d7047e4efef11cdaf2d05d0
zoomkoding <gur5381@gmail.com>
2020-07-24 05:42:27 +0900

    feat: api 그냥 한곳에서 모아서 관리

--- client/src/apis/project.js
+++
@@ -1,8 +0,0 @@
-import { GET, POST, PUT } from '.';
-
-export default {
-  getProject: async (id) => await await GET(`/projects/${id}`),
-  createNote: async (notes) => await (await POST(`/notes`, notes)).json(),
-  moveNote: async (data) => await (await PUT(`/notes/move`, data)).json(),
-  moveGroup: async (data) => await (await PUT(`/groups/move`, data)).json(),
-};
