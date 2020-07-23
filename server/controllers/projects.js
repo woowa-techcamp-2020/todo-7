76910f6328fd4809d20abaaa30a9e7a0d2787075
zoomkoding <gur5381@gmail.com>
2020-07-24 05:47:31 +0900

    feat: 프로젝트 생성 api 구현, 프로젝트 불러올 때 authority도 같이 전달 구현

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -1,17 +1,28 @@
 const Projects = require('../models/projects');
+const Groups = require('../models/groups');
 const Events = require('../models/events');
+const UserProjectRelations = require('../models/userProjectRelations');
 
 exports.create = async (req, res) => {
-  const project = await Projects.create(req.body);
-  res.send(project);
+  const project = await Projects.create({ title: req.body.title });
+  await Groups.create({ title: 'To do', projectId: project.id });
+  await Groups.create({ title: 'Doing', projectId: project.id });
+  await Groups.create({ title: 'Done', projectId: project.id });
+  await UserProjectRelations.create(req.body.users, project.id);
+  res.send({ id: project.id });
 };
 
 exports.get = async (req, res) => {
   const project = await Projects.findById({ id: req.params.id });
   if (!project) res.status(404).send('project not found');
   else {
-    const events = await Events.getByProjectId({ id: req.params.id });
-    project.events = events;
+    project.events = await Events.getByProjectId({ id: req.params.id });
+    project.authority = (
+      await UserProjectRelations.findOne('authority', {
+        userId: req.user.id,
+        projectId: req.params.id,
+      })
+    ).authority;
     res.send(project);
   }
 };
