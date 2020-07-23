a340d118c04ec9ef53bc8067f6c40405fca2dee4
younho9 <younho9.choo@gmail.com>
2020-07-23 17:58:35 +0900

    feat: modal 컴포넌트 추가

---
+++ client/src/components/molecules/modal/index.js
@@ -0,0 +1,86 @@
+import element from '../../../utils/element';
+import button from '../../atoms/button';
+import textarea from '../../atoms/textarea';
+import icon from '../../atoms/icon';
+import input from '../../atoms/input';
+import header from '../header';
+import './styles.css';
+
+const projectColumnEditModal = (className, data) => [
+  element({
+    className: `${className} modal-content`,
+    child: [
+      header({
+        className: `${className}-header modal-header`,
+        leading: element({
+          className: `${className}-header-text`,
+          child: `Edit ${data.title}`,
+        }),
+        actions: element({
+          className: `${className}-header-actions`,
+          child: [icon({ className: `${className}-header-delete-icon` })],
+        }),
+      }),
+      element({
+        className: `${className}-body modal-body`,
+        child: [
+          `Column name`,
+          input({
+            className: `${className}-body-input`,
+            value: `${data.title}`,
+          }),
+          button({
+            className: `${className}-update-button primary`,
+            text: 'Update column',
+          }),
+        ],
+      }),
+    ],
+  }),
+];
+
+const projectColumnCardEditModal = (className, data) => [
+  element({
+    className: `${className}-modal-content`,
+    child: [
+      header({
+        className: `${className}-header modal-header`,
+        leading: element({
+          className: `${className}-header-text`,
+          child: `Edit note`,
+        }),
+        actions: icon({ className: `${className}-header-delete-icon` }),
+      }),
+      element({
+        className: `${className}-body modal-body`,
+        child: [
+          `Note`,
+          textarea({
+            className: `${className}-body-textarea`,
+            value: `${data.title}`,
+          }),
+          button({
+            className: `${className}-update-button primary`,
+            text: 'Save note',
+          }),
+        ],
+      }),
+    ],
+  }),
+];
+
+const getModalByClassName = (className, data) => {
+  switch (className) {
+    case 'project-column-edit-modal':
+      return projectColumnEditModal(className, data);
+    case 'project-column-card-edit-modal':
+      return projectColumnCardEditModal(className, data);
+  }
+};
+
+export default ({ className, data }) =>
+  element({
+    className: `${className} modal`,
+    id: `${className}-${data?.id}`,
+    child: getModalByClassName(className, data),
+  });
