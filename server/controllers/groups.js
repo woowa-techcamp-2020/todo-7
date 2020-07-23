47bd86c5e52b4de1dc8b7141a69582764f47f9d2
younho9 <younho9.choo@gmail.com>
2020-07-23 11:19:14 +0900

    refactor: 이벤트 타이틀에 card, column 인지 추가

--- server/controllers/groups.js
+++ server/controllers/groups.js
@@ -3,7 +3,7 @@ const Events = require('../models/events');
 
 exports.create = async (req, res) => {
   const group = await Groups.create(req.body);
-  const event = await Events.create({ projectId: req.body.projectId, title: `created ${req.body.title}` });
+  const event = await Events.create({ projectId: req.body.projectId, title: `created column ${req.body.title}` });
   res.send({ group, event });
 };
 
@@ -15,20 +15,20 @@ exports.findById = async (req, res) => {
 
 exports.update = async (req, res) => {
   await Groups.update(req.body);
-  const event = await Events.create({ projectId: req.body.projectId, title: `updated ${req.body.title}` });
+  const event = await Events.create({ projectId: req.body.projectId, title: `updated column ${req.body.title}` });
   res.send(event);
 };
 
 exports.move = async (req, res) => {
   const group = await Groups.findOne('title', { id: req.body.id });
   await Groups.move(req.body);
-  const event = await Events.create({ projectId: req.body.projectId, title: `moved ${group.title}` });
+  const event = await Events.create({ projectId: req.body.projectId, title: `moved column ${group.title}` });
   res.send(event);
 };
 
 exports.delete = async (req, res) => {
   const group = await Groups.findOne('title', { id: parseInt(req.query.id) });
   await Groups.delete(parseInt(req.query.id));
-  const event = await Events.create({ projectId: parseInt(req.query.projectId), title: `deleted ${group.title}` });
+  const event = await Events.create({ projectId: parseInt(req.query.projectId), title: `deleted column ${group.title}` });
   res.send(event);
 };
--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -3,7 +3,7 @@ const Events = require('../models/events');
 
 exports.create = async (req, res) => {
   const note = await Notes.create(req.body);
-  const event = await Events.create({ projectId: req.body.projectId, title: `created ${req.body.title}` });
+  const event = await Events.create({ projectId: req.body.projectId, title: `created card ${req.body.title}` });
   res.send({ note, event });
 };
 
@@ -15,7 +15,7 @@ exports.findById = async (req, res) => {
 
 exports.update = async (req, res) => {
   await Notes.update(req.body);
-  const event = await Events.create({ projectId: req.body.projectId, title: `updated ${req.body.title}` });
+  const event = await Events.create({ projectId: req.body.projectId, title: `updated card ${req.body.title}` });
   res.send(event);
 };
 
@@ -25,7 +25,7 @@ exports.move = async (req, res) => {
   if (note.groupTitle === req.body.groupTitle) {
     res.send(200);
   } else {
-    const event = await Events.create({ projectId: req.body.projectId, title: `moved ${note.title} from ${note.groupTitle} to ${req.body.groupTitle}` });
+    const event = await Events.create({ projectId: req.body.projectId, title: `moved card ${note.title} from ${note.groupTitle} to ${req.body.groupTitle}` });
     res.send(event);
   }
 };
@@ -33,6 +33,6 @@ exports.move = async (req, res) => {
 exports.delete = async (req, res) => {
   const note = await Notes.findOne('title', { id: parseInt(req.query.id) });
   await Notes.delete(parseInt(req.query.id));
-  const event = await Events.create({ projectId: parseInt(req.query.projectId), title: `deleted ${note.title}` });
+  const event = await Events.create({ projectId: parseInt(req.query.projectId), title: `deleted card ${note.title}` });
   res.send(event);
 };
