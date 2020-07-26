71d69ab825f12044bca4d4f108294c075cfb48fe
younho9 <younho9.choo@gmail.com>
2020-07-26 15:46:25 +0900

    chore: 불필요한 주석 삭제

--- client/src/components/atoms/icon/index.js
+++ client/src/components/atoms/icon/index.js
@@ -2,10 +2,6 @@ import element from '../../../utils/element';
 
 import './styles.css';
 
-export default ({ className = 'default', type = 'primary', areaHidden = false }) =>
-  // element({
-  //   className: `${className} icon`,
-  {
-    return `<i class='${className} ${type} icon' ${areaHidden ? "area-hidden='true'" : ''}'></i>`;
-  };
-// });
+export default ({ className = 'default', type = 'primary', areaHidden = false }) => {
+  return `<i class='${className} ${type} icon' ${areaHidden ? "area-hidden='true'" : ''}'></i>`;
+};
