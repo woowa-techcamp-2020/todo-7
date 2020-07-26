c3f5b125f0fe173f396d4c56a609a8a2441a5745
zoomkoding <zoomkoding@gmail.com>
2020-07-26 16:56:04 +0900

    fix: note 추가시 시간 생성시 발생하는 버그 해결

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -4,7 +4,7 @@ const { wrapBold } = require('../utils/helper');
 
 exports.create = async (req, res) => {
   const note = await Notes.create(req.body);
-  note.createdAt = new Date.now();
+  note.createdAt = new Date();
   const event = await Events.create({
     projectId: req.body.projectId,
     title: `${wrapBold(req.user.nickname)} created card ${wrapBold(req.body.title)}`,
